c347c5efe0d4f9c5bcb95407f6b3be5927ea4107
zouchaoqun <zouchaoqun@gmail.com>
2008-12-30 00:49:05 +0800

    Add new directory: lib

---
+++ lib/attachment_patch.rb
@@ -0,0 +1,35 @@
+# This patch's original author is edavis10.
+# This patch is downloaded from his redmine-budget-plugin.
+# http://github.com/edavis10/redmine-budget-plugin/tree/master
+#
+
+require_dependency 'attachment'
+
+# Patches Redmine's Attachments dynamically. Adds a relationship
+# Attachment +belongs_to+ to Faq
+module AttachmentPatch
+  def self.included(base) # :nodoc:
+    base.extend(ClassMethods)
+
+    base.send(:include, InstanceMethods)
+
+    # Same as typing in the class
+    base.class_eval do
+      unloadable # Send unloadable so it will not be unloaded in development
+      belongs_to :faq
+
+    end
+
+  end
+
+  module ClassMethods
+
+  end
+
+  module InstanceMethods
+
+  end
+end
+
+# Add module to Attachment
+Attachment.send(:include, AttachmentPatch)
