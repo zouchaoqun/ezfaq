45f8502de75a9682ce469428ad22081b65ac7c1b
zouchaoqun <zouchaoqun@gmail.com>
2009-07-13 20:35:13 +0800

    seperate faq delete permission from edit permission

--- init.rb
+++ init.rb
@@ -38,7 +38,8 @@ Redmine::Plugin.register :ezfaq_plugin do
   project_module :ezfaq do
     permission :view_faqs, {:ezfaq => [:index, :show, :history, :diff, :show_history_version]}, :public => true
     permission :add_faqs, {:ezfaq => [:new, :preview]}, :require => :loggedin
-    permission :edit_faqs, {:ezfaq => [:edit, :preview, :copy, :destroy, :list_invalid_faqs]}, :require => :member
+    permission :edit_faqs, {:ezfaq => [:edit, :preview, :copy, :list_invalid_faqs]}, :require => :member
+    permission :delete_faqs, {:ezfaq => [:destroy]}, :require => :member
     permission :manage_faq_categories, {:ezfaq => [:add_faq_category], :faq_categories => [:index, :change_order, :edit, :destroy]}, :require => :member
     permission :faq_setting, {:ezfaq => [:faq_setting]}, :require => :member
   end
